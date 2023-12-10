89b60bccef54a1d3745fd65f38f997977b845239
AhmedRaed0097 <83637337+AhmedRaed0097@users.noreply.github.com>
2023-12-10 16:45:47 +0300

    add useTranslateRoutePath composable

---
+++ composables/useTranslateRoutePath.js
@@ -0,0 +1,87 @@
+import { useRoute } from "vue-router";
+
+export default function useTranslateRoutePath() {
+  const route = useRoute();
+  const uuidRegex = /[a-f\d]{8}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{12}/i;
+
+  const translatedRoute = ref(null);
+  const translatePath = (path) => {
+    const words = path;
+    if (words.length) {
+      switch (words) {
+        case "index":
+          return "الرئيسية";
+        case "dashboards":
+          return "لوحات التحكم";
+        case "doctors":
+          return "الاطباء";
+        case "customers":
+          return "العملاء";
+        case "permissions-users":
+          return "صلاحيات المستخدمين";
+        case "permissions-employees":
+          return "صلاحيات الموظفين";
+        case "qualifications":
+          return "المؤهلات العلمية";
+        case "specialties":
+          return "التخصصات";
+        case "professional-titles":
+          return "المسميات المهنية";
+        case "classifications":
+          return "التصنيفات";
+        case "categories":
+          return "الفئات";
+        case "general-settings":
+          return "الإعدادات العامة";
+        case "rates":
+          return "التقييمات";
+        case "consulting-sessions":
+          return "الجلسات";
+        case "promotes":
+          return "العروض الترويجية";
+        case "blog-categories":
+          return "اقسام المدونة";
+        case "articles":
+          return "المقالات";
+        case "categories":
+          return "الفئات";
+        case "support-tickets":
+          return "تذاكر الدعم";
+        case "attendance-appointments":
+          return "المواعيد الحضورية";
+        case "transaction":
+          return "المعاملات المالية";
+        case "wallets":
+          return "المحافظ";
+        case "auth-Login":
+          return "تسجيل الدخول";
+        default:
+          return words;
+      }
+    } else {
+      return "الرئيسية";
+    }
+  };
+
+  const setHeadTitle = (routePath) => {
+    translatedRoute.value = translatePath(routePath);
+    useHead({
+      meta: [{ content: translatedRoute.value }],
+      titleTemplate: (titleChunk) => {
+        return `Ucan - ${translatedRoute.value}`;
+      },
+    });
+  };
+
+  translatedRoute.value = translatePath(route.name);
+
+  watch(route, (newRoute) => {
+    // Do something with the new value
+    translatedRoute.value = translatePath(newRoute.name);
+  });
+
+  return {
+    translatedRoute,
+    setHeadTitle,
+  };
+}
